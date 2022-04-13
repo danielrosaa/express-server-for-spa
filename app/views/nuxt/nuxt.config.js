0f1e31fda710603eb551f575e2ac70e030c3ff1a
Daniel Rosa <danielrosaa@gmail.com>
2022-04-13 18:06:31 -0300

    Changes titles"

--- app/views/nuxt/nuxt.config.js
+++ app/views/nuxt/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'front1',
+    title: 'Nuxt',
     htmlAttrs: {
       lang: 'en'
     },
